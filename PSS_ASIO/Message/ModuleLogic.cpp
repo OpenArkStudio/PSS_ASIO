df24593d8d6b8155c89bd008e16ded879eaed045
freeeyes <freeeyes@163.com>
2021-04-02 15:10:47 +0800

    fix sonar code smell.

--- PSS_ASIO/Message/ModuleLogic.cpp
+++ PSS_ASIO/Message/ModuleLogic.cpp
@@ -349,23 +349,25 @@ void CWorkThreadLogic::do_work_thread_module_logic(shared_ptr<ISession> session,
         auto curr_send_packet = std::make_shared<CMessage_Packet>();
         module_logic->do_thread_module_logic(source, recv_packet, curr_send_packet);
 
-        if (curr_send_packet->buffer_.size() > 0)
+        if (curr_send_packet->buffer_.size() == 0)
         {
-            //在这里添加对curr_send_packet的格式化
-            if (module_logic->get_session_interface(connect_id)->is_need_send_format() == true)
-            {
-                auto curr_format_send_packet = std::make_shared<CMessage_Packet>();
-                if(true == module_logic->get_session_interface(connect_id)->format_send_packet(source.connect_id_, curr_send_packet, curr_format_send_packet))
-                {
-                    //将格式化后的数据填充到send_packet
-                    send_packet.buffer_.append(curr_format_send_packet->buffer_.c_str(), curr_format_send_packet->buffer_.size());
-                }
-            }
-            else
+            continue;
+        }
+
+        //在这里添加对curr_send_packet的格式化
+        if (module_logic->get_session_interface(connect_id)->is_need_send_format() == true)
+        {
+            auto curr_format_send_packet = std::make_shared<CMessage_Packet>();
+            if(true == module_logic->get_session_interface(connect_id)->format_send_packet(source.connect_id_, curr_send_packet, curr_format_send_packet))
             {
                 //将格式化后的数据填充到send_packet
-                send_packet.buffer_.append(curr_send_packet->buffer_.c_str(), curr_send_packet->buffer_.size());
+                send_packet.buffer_.append(curr_format_send_packet->buffer_.c_str(), curr_format_send_packet->buffer_.size());
             }
+        }
+        else
+        {
+            //将格式化后的数据填充到send_packet
+            send_packet.buffer_.append(curr_send_packet->buffer_.c_str(), curr_send_packet->buffer_.size());
         }
     }
 
