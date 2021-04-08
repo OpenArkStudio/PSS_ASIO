c7854575fb140753a6d37497726f4ddf8af0c151
freeeyes <freeeyes@163.com>
2021-04-08 10:03:33 +0800

    add test gcov code coverage.

--- Module_Logic/Test_Logic/BaseCommand.cpp
+++ Module_Logic/Test_Logic/BaseCommand.cpp
@@ -5,21 +5,35 @@ void CBaseCommand::Init(ISessionService* session_service)
     session_service_ = session_service;
 
 #ifdef GCOV_TEST
-        session_service_->create_frame_work_thread(plugin_test_logic_thread_id);
+    session_service_->create_frame_work_thread(plugin_test_logic_thread_id);
 
-        auto send_message = std::make_shared<CMessage_Packet>();
-        CFrame_Message_Delay delay_timer;
+    auto send_message = std::make_shared<CMessage_Packet>();
+    CFrame_Message_Delay delay_timer;
 
-        delay_timer.delay_seconds_ = std::chrono::seconds(1);
-        delay_timer.timer_id_ = 1001;  //这个ID必须是全局唯一的
+    delay_timer.delay_seconds_ = std::chrono::seconds(1);
+    delay_timer.timer_id_ = 1001;  //这个ID必须是全局唯一的
 
-        send_message->command_id_ = COMMAND_TEST_FRAME;
-        send_message->buffer_ = "freeeyes";
-        session_service_->send_frame_message(plugin_test_logic_thread_id, "time loop", send_message, delay_timer);
+    send_message->command_id_ = COMMAND_TEST_FRAME;
+    send_message->buffer_ = "freeeyes";
+    session_service_->send_frame_message(plugin_test_logic_thread_id, "time loop", send_message, delay_timer);
 
-        session_service_->run_work_thread_logic(plugin_test_logic_thread_id, delay_timer, [this](){
-            PSS_LOGGER_DEBUG("[run_work_thread_logic]arrived.");
-            });
+    session_service_->run_work_thread_logic(plugin_test_logic_thread_id, delay_timer, [this]() {
+        PSS_LOGGER_DEBUG("[run_work_thread_logic]arrived.");
+        });
+
+    //测试数据透传接口
+    _ClientIPInfo from_io;
+    from_io.m_strClientIP = "127.0.0.1";
+    from_io.m_u2Port = 10010;
+
+    _ClientIPInfo to_io;
+    from_io.m_strClientIP = "127.0.0.1";
+    from_io.m_u2Port = 10003;
+
+    session_service_->add_session_io_mapping(from_io,
+        EM_CONNECT_IO_TYPE::CONNECT_IO_SERVER_TCP,
+        to_io,
+        EM_CONNECT_IO_TYPE::CONNECT_IO_SERVER_TCP);
 #endif
 
     PSS_LOGGER_DEBUG("[load_module]({0})io thread count.", session_service_->get_io_work_thread_count());
@@ -124,25 +138,34 @@ void CBaseCommand::logic_test_frame(const CMessage_Source& source, std::shared_p
     PSS_LOGGER_DEBUG("[logic_test_frame] tag_name={0},data={1}.", source.remote_ip_.m_strClientIP, recv_packet->buffer_);
 
 #ifdef GCOV_TEST
-        auto send_message = std::make_shared<CMessage_Packet>();
-        CFrame_Message_Delay delay_timer;
+    auto send_message = std::make_shared<CMessage_Packet>();
+    CFrame_Message_Delay delay_timer;
 
-        delay_timer.delay_seconds_ = std::chrono::seconds(5);
-        delay_timer.timer_id_ = 1001;  //这个ID必须是全局唯一的
+    //测试创建工作线程
+    session_service_->create_frame_work_thread(1010);
 
-        send_message->command_id_ = COMMAND_TEST_FRAME;
-        send_message->buffer_ = "freeeyes";
-        session_service_->send_frame_message(plugin_test_logic_thread_id, "time loop", send_message, delay_timer);
+    //测试投递线程
+    delay_timer.delay_seconds_ = std::chrono::seconds(5);
+    delay_timer.timer_id_ = 1002;  //这个ID必须是全局唯一的
 
-        //测试定时器(删除)
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        session_service_->delete_frame_message_timer(1001);
+    send_message->command_id_ = COMMAND_TEST_FRAME;
+    send_message->buffer_ = "freeeyes";
+    session_service_->run_work_thread_logic(1010, delay_timer, []() {
+        PSS_LOGGER_DEBUG("[run_work_thread_logic]1010 is arrived.");
+        });
+
+    //测试定时器(删除)
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    session_service_->delete_frame_message_timer(1002);
+
+    //测试关闭工作线程
+    session_service_->close_frame_work_thread(1010);
 #endif
 }
 
 void CBaseCommand::logic_test_connect_error(const CMessage_Source& source, std::shared_ptr<CMessage_Packet> recv_packet, std::shared_ptr<CMessage_Packet> send_packet)
 {
-    PSS_LOGGER_DEBUG("[CBaseCommand::logic_test_connect_error]{0}:{1}", 
+    PSS_LOGGER_DEBUG("[CBaseCommand::logic_test_connect_error]{0}:{1}",
         source.remote_ip_.m_strClientIP,
         source.remote_ip_.m_u2Port);
 }
@@ -170,12 +193,14 @@ void CBaseCommand::logic_http_websocket_shark_hand(const CMessage_Source& source
 
 void CBaseCommand::logic_http_websocket_data(const CMessage_Source& source, std::shared_ptr<CMessage_Packet> recv_packet, std::shared_ptr<CMessage_Packet> send_packet)
 {
-    PSS_LOGGER_DEBUG("[logic_http_websocket_data]text data={0}", recv_packet->buffer_);
+    PSS_LOGGER_DEBUG("[logic_http_websocket_data]source={0}, text data={1}", source.connect_id_, recv_packet->buffer_);
     //返回websocket消息(同步测试)
     //send_packet.buffer_ = recv_packet.buffer_;
 
     //返回websocket消息(异步测试)
     auto send_asyn_packet = std::make_shared<CMessage_Packet>();
     send_asyn_packet->buffer_.append(recv_packet->buffer_.c_str(), recv_packet->buffer_.size());
+
     session_service_->send_io_message(source.connect_id_, send_asyn_packet);
+
 }
