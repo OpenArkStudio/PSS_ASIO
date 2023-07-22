c28722a4b7248664fbbeb99ce8bfaf234517c6b9
liuchao412 <liuchao412@foxmail.com>
2023-07-22 15:22:35 +0800

    Support operating network service interfaces for logic.

--- PSS_ASIO/PSS_ASIO.cpp
+++ PSS_ASIO/PSS_ASIO.cpp
@@ -2,6 +2,40 @@
 //
 
 #include "ServerService.h"
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+string GetNameByPid(pid_t pid) 
+{
+    char proc_pid_path[BUF_SIZE] = {0};
+    char buf[BUF_SIZE] = {0};
+    char task_name[BUF_SIZE] = {0};
+    string strTaskName = "";
+
+    sprintf(proc_pid_path, "/proc/%d/status", pid);
+    FILE* fp = fopen(proc_pid_path, "r");
+    if(NULL != fp)
+    {
+        if( fgets(buf, BUF_SIZE-1, fp) == nullptr)
+        {
+            fclose(fp);
+        }
+        else
+        {
+            fclose(fp);
+            sscanf(buf, "%*s %s", task_name);
+        }
+        
+        strTaskName = task_name;
+    }
+
+    return strTaskName;
+}
 
 int main(int argc, char* argv[])
 {
@@ -15,11 +49,13 @@ int main(int argc, char* argv[])
     //如果存在配置文件名称，则读取指定的配置文件
     if (argc == 2)
     {
-        App_ServerService::instance()->init_servce(argv[1]);
+        App_ServerService::instance()->init_service(argv[1]);
     }
     else
     {
-        App_ServerService::instance()->init_servce();
+        string strAppName = GetNameByPid(getpid());
+        string strCfgFile = strAppName + ".json"; 
+        App_ServerService::instance()->init_service(strCfgFile);
     }
     return 0;
 }
