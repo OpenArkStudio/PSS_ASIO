69cb6f4f48a8cad096664b02c1c0595ddea27302
freeeyes <freeeyes@163.com>
2024-02-27 12:03:26 +0800

    add test data.

--- tests/test_Iotoio.cpp
+++ tests/test_Iotoio.cpp
@@ -15,23 +15,8 @@ int run_test_iotoio()
     to_io.m_u2Port = 10003;
 
     io_to_io.add_session_io_mapping(from_io, from_io_type, to_io, to_io_type, ENUM_IO_BRIDGE_TYPE::IO_BRIDGE_BATH);
-    io_to_io.regedit_bridge_session_id(from_io, from_io_type, 1);
-    if (io_to_io.get_to_session_id(1, from_io) != 0)
-    {
-        return 1;
-    }
-
-    io_to_io.regedit_bridge_session_id(to_io, to_io_type, 2);
-    if (io_to_io.get_to_session_id(1, from_io) != 2)
-    {
-        return 1;
-    }
 
     io_to_io.delete_session_io_mapping(to_io, to_io_type);
-    if (io_to_io.get_to_session_id(1, from_io) == 0)
-    {
-        return 1;
-    }
 
     return 0;
 }
