aec5866c14def58d843c115288b35cf97887653d
freeeyes <freeeyes@163.com>
2025-01-10 14:00:26 +0800

    update test.

--- PSS_ASIO_CLIENT/PSS_ASIO_CLIENT.cpp
+++ PSS_ASIO_CLIENT/PSS_ASIO_CLIENT.cpp
@@ -403,11 +403,12 @@ int main()
     tcp_test_connect_synchronize_server("127.0.0.1", 10002, 10010, 0x2101, 1, io_context);
     tcp_test_connect_synchronize_server("127.0.0.1", 10002, 10011, 0x2102, 1, io_context);
 
-    std::cout << "[udp_test_connect_synchronize_server]udp 0x2101 test ok." << std::endl;
-    udp_test_connect_synchronize_server("127.0.0.1", 10005, 10012, 0x2101, io_context);
-    std::cout << "[udp_test_connect_synchronize_server]udp 0x2102 test ok." << std::endl;
+    //std::cout << "[udp_test_connect_synchronize_server]udp 0x2101 test ok." << std::endl;
+    //udp_test_connect_synchronize_server("127.0.0.1", 10005, 10012, 0x2101, io_context);
+    
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
     udp_test_connect_synchronize_server("127.0.0.1", 10005, 10012, 0x2102, io_context);
+    std::cout << "[udp_test_connect_synchronize_server]udp 0x2102 test ok." << std::endl;
 
     from_server_get_kcp_id("127.0.0.1", 10100);
 
